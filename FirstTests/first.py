7fa97899290e75d99fa13e8baed9b583081924a3
DrakeAusir <info.ramirocalo@gmail.com>
2021-04-14 12:29:57 +0200

    Primi Test completati, si segue documentazione

--- FirstTests/first.py
+++ FirstTests/first.py
@@ -16,14 +16,22 @@ def testFunction(var):
 	return var
 
 ### Definizione delle Routes
-@app.route('/')
+@app.route('/', methods=['GET',' POST'])
 def rootPage():
     return indexPage()
 
 @app.route('/user/', methods=['GET',' POST'])
 def userPage():
-	username = request.args.get('username')
-	return render_template('user.html', name=username)
+	if request.method == 'GET':
+		dati = [
+			request.args.get('username'),
+			request.args.get('lastname'),
+			request.args.get('phone'),
+			request.args.get('ciao'),	
+			]
+		return render_template('user.html', dati=dati)
+	elif request.method == 'POST':
+		return 'Ciao'
 
 	### definizione 404
 @app.errorhandler(404)
